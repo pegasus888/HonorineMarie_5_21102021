ac88b25ebffbd204daffc4fad1d9d9de79313afc
pegasus888 <ceres.14y@gmail.com>
2021-10-21 18:11:45 +0200

    layout colors

--- js/product.js
+++ js/product.js
@@ -1,3 +1,11 @@
 let params = (new URL(document.location)).searchParams;
 let id = params.get('id'); // is the string "Jonathan Smith".
 console.log(id);
+
+// Get info for a single product
+fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(products => {
+
+
+    console.log(products)
