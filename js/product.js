0db3796c2dd5057d4ce7fd7814a5d85e4201dfc3
pegasus888 <ceres.14y@gmail.com>
2021-10-22 05:25:57 +0200

    product.js

--- js/product.js
+++ js/product.js
@@ -1,11 +1,46 @@
+// Get the product's id
 let params = (new URL(document.location)).searchParams;
-let id = params.get('id'); // is the string "Jonathan Smith".
+let id = params.get('id');
 console.log(id);
 
-// Get info for a single product
-fetch('http://localhost:3000/api/products')
+
+fetch('http://localhost:3000/api/products/' + id)
     .then(response => response.json())
-    .then(products => {
+    .then(items => {
 
+const items = [... document.querySelectorAll(".item")];
 
-    console.log(products)
+// Get information for a single product
+    let result = '';
+items.forEach(item => {
+    result += `
+    <section class="item">
+    <article>
+        <div class="item__img">
+            <img src=${product.imageUrl} alt=${product.altTxt}>
+        </div>
+        <div class="item__content">
+
+            <div class="item__content__titlePrice">
+            <h1 id=${product.name}</h1>
+            <p>Prix : <span id=${product.price}</span>€</p>
+            </div>
+
+            <div class="item__content__description">
+            <p class="item__content__description__title">Description :</p>
+            <p id=${product.description}</p>
+            </div>
+
+            <div class="item__content__settings">
+            <div class="item__content__settings__color">
+                <label for="color-select">Choisir une couleur :</label>
+                <select name="color-select" id="colors">
+                    <option value="">--SVP, choisissez une couleur --</option>
+                    <option value=${product.colors}</option>
+                </select>
+            </div>
+    `
+})
+console.log(result);
+document.getElementsByClassName("item").innerHTML=result;
+    })
