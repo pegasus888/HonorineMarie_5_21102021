9c5ef3274a7df1a1917a3d6b0592cfd22877385a
pegasus888 <ceres.14y@gmail.com>
2021-10-27 04:49:50 +0200

    Display a single product by id

--- js/product.js
+++ js/product.js
@@ -1,42 +1,38 @@
+const products = document.getElementsByClassName("item");
+const productsDetails = document.getElementsByTagName("article");
+
+const productImage = document.querySelector(".item__img");
+const productTitle = document.getElementById("title");
+const productPrice = document.getElementById("price");
+const productDescription = document.getElementById("description");
+const productColor = document.getElementById("colors");
+
+
+// Products added to Local storage
+class Storage {
+    static saveProducts(products){
+        localStorage.setItem("products",JSON.stringify(products));
+    }
+}
+
+Storage.saveProducts(products);
+
+
 // Get the product's id from the URL
 let params = (new URL(document.location)).searchParams;
 let id = params.get('id');
-console.log(id);
 
 
 // Get a single product by id from the API
 fetch(`http://localhost:3000/api/products/${id}`)
     .then(response => response.json())
     .then(productsDetails => {
+        productImage.innerHTML = `<img src=${productsDetails.imageUrl}>`
+        productTitle.textContent = `${productsDetails.name}`
+        productPrice.textContent = `${productsDetails.price}`
+        productDescription.textContent = `${productsDetails.description}`
+        productColor.innerHTML = productsDetails.colors
+            .map((a) => `<option>${a}</option>`)
 
-
-        // Products added to Local storage
-        class Storage {
-            static saveProducts(products){
-                localStorage.setItem("products",JSON.stringify(products));
-            }
-        }
-
-        Storage.saveProducts(products);
-
-
-        // Display a single product on product.html
-            let result = '';
-        productsDetails.forEach(product => {
-            result += `
-                    <img src=${product.imageUrl} alt=${product.altTxt}>
-                    <h1 id=${product.name}</h1>
-                    <p>Prix : <span id=${product.price}</span>€</p>
-                    <p id=${product.description}</p>
-                    <option value=${product.colors}</option>
-                    `
-        })
-        console.log(result);
-        document.getElementsByClassName("item__img").innerHTML=result;
-        document.getElementsById("title").innerHTML=result;
-        document.getElementsById("price").innerHTML=result;
-        document.getElementsById("description").innerHTML=result;
-        document.getElementsById("colors").innerHTML=result;
-
-
-})
+                    })
+    console.log(productsDetails);
