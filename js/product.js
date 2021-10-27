8a61b8bc56d76a953c633650c8d6a83c7e09c1f6
pegasus888 <ceres.14y@gmail.com>
2021-10-27 03:13:05 +0200

    Products added to Local storage

--- js/product.js
+++ js/product.js
@@ -10,24 +10,33 @@ fetch(`http://localhost:3000/api/products/${id}`)
     .then(productsDetails => {
 
 
-console.log(productsDetails);
+        // Products added to Local storage
+        class Storage {
+            static saveProducts(products){
+                localStorage.setItem("products",JSON.stringify(products));
+            }
+        }
+
+        Storage.saveProducts(products);
 
 
-// Display a single product on product.html
-    let result = '';
-productsDetails.forEach(product => {
-    result += `
-            <img src=${product.imageUrl} alt=${product.altTxt}>
-            <h1 id=${product.name}</h1>
-            <p>Prix : <span id=${product.price}</span>€</p>
-            <p id=${product.description}</p>
-            <option value=${product.colors}</option>
-            `
-})
-console.log(result);
-document.getElementsByClassName("item__img").innerHTML=result;
-document.getElementsById("title").innerHTML=result;
-document.getElementsById("price").innerHTML=result;
-document.getElementsById("description").innerHTML=result;
-document.getElementsById("colors").innerHTML=result;
+        // Display a single product on product.html
+            let result = '';
+        productsDetails.forEach(product => {
+            result += `
+                    <img src=${product.imageUrl} alt=${product.altTxt}>
+                    <h1 id=${product.name}</h1>
+                    <p>Prix : <span id=${product.price}</span>€</p>
+                    <p id=${product.description}</p>
+                    <option value=${product.colors}</option>
+                    `
+        })
+        console.log(result);
+        document.getElementsByClassName("item__img").innerHTML=result;
+        document.getElementsById("title").innerHTML=result;
+        document.getElementsById("price").innerHTML=result;
+        document.getElementsById("description").innerHTML=result;
+        document.getElementsById("colors").innerHTML=result;
+
+
 })
