027627088ce907abe45a7a71dbeb8beb7129b9e4
pegasus888 <ceres.14y@gmail.com>
2021-11-04 07:25:29 +0100

    confirmation.js

---
+++ js/confirmation.js
@@ -0,0 +1,15 @@
+// Get the order's id from the Url
+function getOrderId() {
+	let url = new URL(window.location.href);
+	let searchParams = new URLSearchParams(url.search);
+	if (searchParams.has("id")) {
+		let id = searchParams.get("id");
+		return id;
+	} else {
+		console.log("Error, no order Id found");
+	}
+}
+window.addEventListener("load", () => {
+	const orderId = document.getElementById("orderId");
+	orderId.innerText = getOrderId();
+});
